28401daca08e6b7444da9c74df6fe7d95d69b80c
Mario Romero <mario.rralvarez@gmail.com>
2023-10-03 03:17:03 -0600

    probando fake api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
  import axios from "axios"
 
 const api = axios.create({
-    baseURL :"http://localhost:5000"
+    baseURL : "https://my-json-server.typicode.com/Majoras13/AluraFlix"
+    /* baseURL :"http://localhost:5000" */ //local
 })
 
 //Categorias
